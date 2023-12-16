e23f600e7495e42957595c7f515fb6607ab52a40
Илья Д. Мякишев <imyakishev@lovit.ru>
2023-12-16 21:01:37 +0300

    base app created, models for user created, password hashing created, routers for user created

---
+++ db/db_user.py
@@ -0,0 +1,16 @@
+from routers.schemas import UserBase
+from db.models import User
+from sqlalchemy.orm.session import Session
+from db.hashingpass import Hash
+
+
+def create_user(db: Session, request: UserBase):
+    new_user = User(
+        username=request.username,
+        email=request.email,
+        password=Hash.bcrypt(request.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user 
---
+++ db/hashingpass.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+
+
+password_cxt = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+class Hash(): 
+    def bcrypt(password: str): 
+        return password_cxt.hash(password)
+    
+    def verify(hashed_password, plain_password): 
+        return password_cxt.verify(plain_password, hashed_password)
